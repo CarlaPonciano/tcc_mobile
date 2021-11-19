84b781faf0727822df15781f1206619695eb03b3
CarlaPonciano <si.carla.ponciano@outlook.com>
2021-11-18 22:32:28 -0300

    Added Map Screen

--- src/screens/Explore/index.js
+++ src/screens/Explore/index.js
@@ -1,11 +1,103 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
 import { styles } from './styles';
+import api from '../../services/api';
 
-export function Explore() {
+export function Explore({ navigation }) {
+  const [initialRegion, setInitialRegion] = useState({ latitude: 0, longitude: 0 })
+  const [currentRegion, setCurrentRegion] = useState(null)
+  const [campaigns, setCampaigns] = useState([])
+
+  const loadCampaigns = async () =>  { 
+    api.get(`campaigns`).then(response => {
+      setCampaigns(response.data);
+    }); 
+  }
+
+  const loadInitialPosition = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+            enableHighAccuracy: true,
+        })
+
+        const { latitude, longitude } = coords
+
+        setCurrentRegion({
+            latitude,
+            longitude,
+            latitudeDelta: 0.04,
+            longitudeDelta: 0.04,
+        })
+
+        setInitialRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+    }
+  }
+
+  function handleRegionChange(region) {
+    setCurrentRegion(region)
+  } 
+
+  useEffect(() => {
+    loadInitialPosition()
+  }, [])
+
+  useEffect(() => {
+    loadCampaigns()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Explorar</Text>
-    </View>
+    <>
+      <MapView
+          onRegionChangeComplete={handleRegionChange}
+          initialRegion={currentRegion}
+          style={styles.map}
+      >
+        <Marker
+          title="Você está aqui!"
+          coordinate={{
+              latitude: initialRegion.latitude,
+              longitude: initialRegion.longitude
+          }}
+        >
+          <MaterialIcons name='my-location' size={30} color='#975516' />
+        </Marker>
+
+        {campaigns.map(campaign => (
+          <Marker
+            key={campaign.id}
+            coordinate={{
+                latitude: campaign.latitude,
+                longitude: campaign.longitude
+            }}
+          >
+            <Image
+                style={styles.image}
+                source={{ uri: campaign.picture }}
+            />
+
+            <Callout onPress={() => {
+                navigation.navigate('Campaign', { campaign: campaign })
+            }}>
+              <View style={styles.callout}>
+                  <Text style={styles.campaignTitle}>{campaign.name}</Text>
+                  <Text style={styles.campaignDescription} numberOfLines={2}>{campaign.description}</Text>
+                  <MaterialIcons style={styles.campaignCTA} name='arrow-forward' size={25} color='#61D27A' />
+              </View>
+            </Callout>
+
+          </Marker>
+        ))}
+      </MapView>
+    </>
   );
 }
--- src/screens/Explore/styles.js
+++ src/screens/Explore/styles.js
@@ -4,5 +4,43 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fefefe'
+    },
+    map: {
+        flex: 1
+    },
+    userMarker: {
+        borderRadius: 10,
+        borderColor: '#975516',
+        borderWidth: 5,
+    },
+    callout: {
+        width: 260,
+        display: 'flex',
+    },
+    image: {
+        width: 75,
+        height: 75,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#975516'
+    },
+    campaignTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: '#975516',
+        margin: 3,
+        textDecorationLine: "underline",
+    },
+    campaignDescription: {
+        textAlign: 'justify',
+        fontSize: 12,
+        color: '#000',
+        marginLeft: 5,
+        marginRight: 5,
+    },
+    campaignCTA:{
+        margin: 3,
+        alignSelf: 'flex-end',
     }
 });
